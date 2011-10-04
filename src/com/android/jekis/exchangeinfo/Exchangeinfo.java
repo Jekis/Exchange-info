c2e8611d7db47dbc56983e2de5fbae3cbfb29e8c
Yevgeny Chorba <jekis01@gmail.com>
2011-10-05 01:19:48 +0300

    Initial commit

---
+++ src/com/android/jekis/exchangeinfo/Exchangeinfo.java
@@ -0,0 +1,13 @@
+package com.android.jekis.exchangeinfo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Exchangeinfo extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
