01b0598812c8017a0e266df16f5907e7f6cfc4e4
Jenechka <jekis@Jenechka.(none)>
2012-02-28 23:07:04 +0200

    Added splash activity.

--- gen/com/android/jekis/exchangeinfo/R.java
+++ gen/com/android/jekis/exchangeinfo/R.java
@@ -11,12 +11,14 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int app_icon=0x7f020000;
+        public static final int icon=0x7f020001;
     }
     public static final class id {
         public static final int ScrollWraper=0x7f050003;
         public static final int actualRate=0x7f050001;
         public static final int banksTable=0x7f050004;
+        public static final int imageView1=0x7f05000e;
         public static final int name=0x7f050000;
         public static final int radioGroupCurrency=0x7f05000a;
         public static final int radioGroupOperation=0x7f050006;
@@ -33,6 +35,7 @@ public final class R {
         public static final int bank_row=0x7f030000;
         public static final int banks=0x7f030001;
         public static final int main=0x7f030002;
+        public static final int splash=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
---
+++ src/com/android/jekis/exchangeinfo/SplashActivity.java
@@ -0,0 +1,52 @@
+package com.android.jekis.exchangeinfo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+
+public class SplashActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        // Create an object of type SplashHandler.
+        SplashHandler mHandler = new SplashHandler();
+        // Set the layout for this activity.
+        this.setContentView(R.layout.splash);
+        // Create a Message object.
+        Message msg = new Message();
+        // Assign a unique code to the message.
+        // Later, this code will be used to identify the message in Handler
+        // class.
+        msg.what = 0;
+        // Send the message with a delay of 3 seconds(3000 = 3 sec).
+        mHandler.sendMessageDelayed(msg, 2000);
+    }
+
+    // Handler class implementation to handle the message.
+    private class SplashHandler extends Handler {
+
+        // This method is used to handle received messages.
+        @Override
+        public void handleMessage(Message msg) {
+            // Switch to identify the message by its code.
+            switch (msg.what) {
+                default:
+                case 0:
+                    super.handleMessage(msg);
+
+                    // Create an intent to start the new activity.
+                    // Our intention is to start MainActivity.
+                    Intent intent = new Intent();
+                    intent.setClass(SplashActivity.this, Exchangeinfo.class);
+                    SplashActivity.this.startActivity(intent);
+                    // Finish the current activity.
+                    SplashActivity.this.finish();
+            }
+        }
+    }
+}
